0d6a35355d0e9de3e1cf60f4e422283e1e47d50d
Alex Adamovic <alexanderadamovic@gmail.com>
2022-01-21 08:27:55 -0800

    add code to pass first test

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// Business Logic
+
+function neighborhood(num) {
+  let numArray =[0];
+  return numArray
+}
+
+
+// UI Logic
+
+$(document).ready(function(){
+  $("form#roboger").submit(function(event){
+    event.preventDefault();
+
+
+
+  });
+});
